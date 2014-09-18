95b5f089efba7a46b2e5bad36591e50cdda9c27a
skyhills13 <skyhills13@gmail.com>
2014-09-18 16:26:08 +0900

    initial commit with url shortener using mysql

---
+++ redisShortener/src/main/java/redisShortener/Logic.java
@@ -0,0 +1,163 @@
+package redisShortener;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.UUID;
+
+public class Logic {
+	
+	public Connection getConnection() throws IllegalAccessException,
+			ClassNotFoundException, SQLException {
+		// adjust it
+		String url = "jdbc:mysql://10.73.45.60/redisShortener";
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(url, "popi", "db1004");
+		return conn;
+	}
+
+	public String getId(String longUrl) throws Exception {
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement st = null;
+
+		String query = "SELECT id FROM url_data WHERE long_url='"
+				+ longUrl.trim() + "'";
+		String id = null;
+		try {
+			try {
+				conn = getConnection();
+				st = conn.createStatement();
+				rs = st.executeQuery(query);
+				if (rs.next()) {
+					id = rs.getString("id");
+				}
+			} finally {
+
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		return id;
+	}
+	
+	public String getShortUrl(String longUrl) throws Exception {
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement st = null;
+
+		String query = "SELECT short_url FROM url_data WHERE long_url='"
+				+ longUrl.trim() + "'";
+		String shortUrl = null;
+		try {
+			try {
+				conn = getConnection();
+				st = conn.createStatement();
+				rs = st.executeQuery(query);
+				if (rs.next()) {
+					shortUrl = rs.getString("short_url");
+				}
+			} finally {
+
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		return shortUrl;
+	}
+	
+	public String makeShort(String longUrl) {
+		return (String)UUID.randomUUID().toString().subSequence(0, 8);
+	}
+
+	public String getShort(String serverName, int port, String contextPath,
+			String longUrl) throws Exception {
+
+		Connection conn = null;
+
+		Statement st = null;
+		String id = getId(longUrl);// check if URL has been shorten already
+		String shortUrl; 
+		if (id != null) {
+			// if id is not null, this link has been shorten already.
+			// nothing to do
+			shortUrl = getShortUrl(longUrl);
+		} else {
+			shortUrl = makeShort(longUrl);
+			// at this point id is null, make it shorter
+			String sqlInsert = "INSERT INTO url_data(long_url, short_url) VALUES('"
+					+ longUrl.trim() + "','" +shortUrl+ "')";
+			try {
+				conn = getConnection();
+				st = conn.createStatement();
+				st.execute(sqlInsert);
+			} finally {
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}
+			// after we insert the record, we obtain the ID as identifier of our
+			// new short link
+			System.out.println(shortUrl + ", " + getShortUrl(longUrl));
+			
+		}
+		return "http://" + serverName + ":" + port + contextPath + "/" + shortUrl;
+	}
+
+	public String getLongUrl(String urlShort) throws Exception {
+		if (urlShort.startsWith("/")) {
+			urlShort = urlShort.replace("/", "");
+		}
+		String query = "SELECT long_url FROM url_data where short_url='" + urlShort + "'";
+		String longUrl = null;
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement st = null;
+
+		try {
+			conn = getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery(query);
+			if (rs.next()) {
+				longUrl = rs.getString("long_url");
+			}
+		} finally {
+
+			if (rs != null) {
+				rs.close();
+			}
+			if (st != null) {
+				st.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		}
+
+		return longUrl;
+	}
+
+}
---
+++ redisShortener/src/main/java/redisShortener/Retrieve.java
@@ -0,0 +1,42 @@
+package redisShortener;
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+public class Retrieve extends HttpServlet {
+ 
+    private static final long serialVersionUID = 1293961717469276130L;
+ 
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+ 
+    String urlShort = request.getServletPath();
+   
+ 
+    String longUrl = null;
+    if (urlShort != null && !"".equals(urlShort)) {
+        try {
+        longUrl = new Logic().getLongUrl(urlShort);
+        } catch (Exception e) {
+        // handling exception here
+        e.printStackTrace();
+        }
+    }
+ 
+    if (longUrl == null) {
+        // if long url not found, send to index.jsp
+        System.out.println("long url not found, back to index.jsp");
+        response.sendRedirect("index.jsp");
+    } else {
+        //if long url found, so redirect the browser
+        System.out.println("redirecting to "+longUrl );
+        response.sendRedirect(longUrl);
+    }
+    }
+ 
+}
