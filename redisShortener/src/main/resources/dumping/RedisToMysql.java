17c3818648a42c08b0908f5770b9586740487935
skyhills13 <skyhills13@gmail.com>
2014-10-01 20:13:02 +0900

    make class for dumping

---
+++ redisShortener/src/main/resources/dumping/RedisToMysql.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import redisShortener.Logic;
+
+public class RedisToMysql {
+
+	private static Logic logic = new Logic();
+
+	public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException, IOException, SQLException {
+		insert("/home/next/Application/tomcat/webapps/redisCommands");
+	}
+	
+	public static void insert(String fileName) throws IOException,
+			IllegalAccessException, ClassNotFoundException, SQLException {
+
+		Connection conn = null;
+		Statement st = null;
+
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		try {
+			String line;
+			line = br.readLine();
+			while (line != null) {
+				String[] array = line.split(",");
+				String longUrl = array[0];
+				String shortUrl = array[1];
+
+				String sqlInsert = "INSERT INTO url_data(long_url, short_url) VALUES('"
+						+ longUrl.trim() + "','" + shortUrl.trim() + "')";
+				try {
+
+					conn = logic.getConnection();
+					st = conn.createStatement();
+					st.execute(sqlInsert);
+					line = br.readLine();
+
+				} finally {
+					if (st != null) {
+						st.close();
+					}
+					if (conn != null) {
+						conn.close();
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
